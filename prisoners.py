f563f8ef5f679a645cc9171642d8e7a3eee2f3fa
mattsoh <67374564+mattsoh@users.noreply.github.com>
2024-08-26 17:31:16 +0800

    Update prisoners.py

--- prisoners.py
+++ prisoners.py
@@ -1,133 +1,143 @@
-#setup
+# Setup
 import os
 from random import randint as rand
-choice=[]
-#modes
+
+choice = []
+
+
+# Modes
 def computer():
     wipe()
-    print("You have chosen to play against the computer")
-    computer_programs=["Random","Tit for tat", "Freidman","Joss"]
+    print("You chose to play against the computer")
+    programs = ["Random", "Tit for Tat", "Friedman", "Joss"]
     print("-------------------------------------------")
     print("Which program would you like to play against?")
-    for i in range(len(computer_programs)+1):
-        if i == len(computer_programs):
-            print(f"Choose for me - {i+1}")
-        else:
-            print(f"{computer_programs[i]} - {i+1}")
-    option=input()
+    for i, program in enumerate(programs):
+        print(f"{program} - {i+1}")
+    option = input()
     if option.isdigit():
-        option=int(option)
-        if option == len(computer_programs):
-            option=rand(0,len(computer_programs)-1)
+        option = int(option)
+        if option == len(programs):
+            option = rand(0, len(programs) - 1)
     else:
         computer()
-    gamelen=int(input("Please enter game length (number of rounds):  "))
-    game("You",str(option),gamelen)
-    presentscore("You",option)
+    game_len = int(input("Enter game length (rounds): "))
+    game("You", programs[option], game_len)
+    present_score("You", option)
+
+
 def twoplayer():
     wipe()
     print("2 player mode selected")
-    player1=input("Please enter player 1 name:  ")
-    player2=input("Please enter player 2 name:  ")
-    gamelen=int(input("Please enter game length (number of rounds):  "))
-    game(player1,player2,gamelen)
-    presentscore(player1,player2)
-#general functions
+    player1 = input("Player 1 name: ")
+    player2 = input("Player 2 name: ")
+    game_len = int(input("Enter game length (rounds): "))
+    game(player1, player2, game_len)
+    present_score(player1, player2)
+
+
+# General functions
 def score():
-    p1score=0
-    p2score=0
-    for i in choice:
-        choices=i[0]+i[1]
+    p1, p2 = 0, 0
+    for choices in choice:
         if choices == "dd":
-            p1score+=1
-            p2score+=1
+            p1 += 1
+            p2 += 1
         elif choices == "cc":
-            p1score+=3
-            p2score+=3
+            p1 += 3
+            p2 += 3
         elif choices == "dc":
-            p1score+=5
+            p1 += 5
         elif choices == "cd":
-            p2score+=5
-    return [p1score,p2score]
+            p2 += 5
+    return p1, p2
+
+
 def wipe():
-    os.system('cls' if os.name =='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def out():
-    for i in choice:
-        print(f"|{i[0]:^2}|{i[1]:^2}|")
+    for moves in choice:
+        print(f"|{moves[0]:^2}|{moves[1]:^2}|")
+
+
 def terminal():
     wipe()
     print("Prisoner's Dilemma")
     print("------------------")
-    print("2 player game - 1")
-    print("Play the computer - 2")
-    print("Hold a Tournament - 3")
-    option=input()
-    if option=="1":
+    print("2 Player Game - 1")
+    print("Play the Computer - 2")
+    print("Hold a Tournament - 3 (Unavailable)")  # Indicate unavailability
+    option = input()
+    if option == "1":
         twoplayer()
-    elif option=="2":
+    elif option == "2":
         computer()
-    elif option=="3":
-        print("option not available yet")
+    elif option == "3":
+        print("Option not available yet")
     else:
         terminal()
-def game(a,b,gamelen):
-    for i in range(gamelen):
-        achoice=play(a,0)
-        bchoice=play(b,1)
-        choice.append([achoice,bchoice])
-def play(p,num):
+
+
+def game(a, b, game_len):
+    for _ in range(game_len):
+        achoice = play(a, 0)
+        bchoice = play(b, 1)
+        choice.append([achoice, bchoice])
+
+
+def play(p, num):
     if p.isdigit():
-        p=int(p)
-        if p==1:
-            c=random()
-        elif p==2:
-            c=t4t(num)
-        elif p==3:
-            c=Freidman(num)
-        elif p==4:
-            c=Joss()
+        p = int(p)
+        if p == 1:
+            return chr(rand(99, 100))
+        elif p == 2:
+            return t4t(num)
+        elif p == 3:
+            return Friedman(num)
+        elif p == 4:
+            return Joss()
         else:
             computer()
     else:
-        c=player(p)
-    return c
-def presentscore(player1,player2):
+        return player(p)
+
+
+def present_score(player1, player2):
     wipe()
     out()
-    n=score()
-    if n[0]>n[1]:
-        print(f"{player1} won {n[0]}-{n[1]}")
-    elif n[1]>n[0]:
-        print(f"{player2} won {n[1]}-{n[0]}")
+    scores = score()
+    if scores[0] > scores[1]:
+        print(f"{player1} won {scores[0]}-{scores[1]}")
+    elif scores[1] > scores[0]:
+        print(f"{player2} won {scores[1]}-{scores[0]}")
     else:
-        print(f"You drew {n[0]}-{n[1]}")
-    
-#programs
+        print(f"You Drew {scores[0]}-{scores[1]}")
+
+
+# Programs
 def player(p):
     wipe()
     print(p)
     print("|p1|p2|")
     out()
-    ch=input("defect (d) or cooperate (c):  ")
-    if ch=="d" or ch=="c":
+    ch = input("Defect (d) or Cooperate (c): ")
+    if ch in ("d", "c"):
         return ch
     else:
         return player(p)
+
+
 def t4t(num):
-    if len(choice)>0:
-        return choice[-1][num-1]
+    if choice:
+        return choice[-1][num - 1]
     return "c"
+
+
 def random():
-    return chr(rand(99,100))
-def Freidman(num):
-    a=[]
-    for i in choice:
-        a.append(i[num-1])
-    if "d" in a:
-        print("d")
-        return "d"
-    print("c")
-    return "c"
-def Joss():
-    return chr(round(rand(994,999)/10))
-terminal()
+    return chr(rand(99, 100))
+
+
+def Friedman(num):
+    actions = [move[num - 1]
