824884552c7b3cc2e29ce91762bb48717cebe5be
mattsoh <67374564+mattsoh@users.noreply.github.com>
2024-08-26 17:02:45 +0800

    Update prisoners.py

--- prisoners.py
+++ prisoners.py
@@ -1,5 +1,29 @@
+#setup
 import os
+from random import randint as rand
 choice=[]
+#modes
+def computer():
+    wipe()
+    print("You have chosen to play against the computer")
+    computer_programs=["Random","Tit for tat", "Freidman","Joss"]
+    print("-------------------------------------------")
+    print("Which program would you like to play against?")
+    for i in range(len(computer_programs)+1):
+        if i == len(computer_programs):
+            print(f"Choose for me - {i+1}")
+        else:
+            print(f"{computer_programs[i]} - {i+1}")
+    option=input()
+    if option.isdigit():
+        option=int(option)
+        if option == len(computer_programs):
+            option=rand(0,len(computer_programs)-1)
+    else:
+        computer()
+    gamelen=int(input("Please enter game length (number of rounds):  "))
+    game("You",str(option),gamelen)
+    presentscore("You",option)
 def twoplayer():
     wipe()
     print("2 player mode selected")
@@ -7,15 +31,8 @@ def twoplayer():
     player2=input("Please enter player 2 name:  ")
     gamelen=int(input("Please enter game length (number of rounds):  "))
     game(player1,player2,gamelen)
-    wipe()
-    out()
-    n=score()
-    if n[0]>n[1]:
-        print(f"{player1} won {n[0]}-{n[1]}")
-    elif n[1]>n[0]:
-        print(f"{player2} won {n[1]}-{n[0]}")
-    else:
-        print(f"You drew {n[0]}-{n[1]}")
+    presentscore(player1,player2)
+#general functions
 def score():
     p1score=0
     p2score=0
@@ -48,7 +65,7 @@ def terminal():
     if option=="1":
         twoplayer()
     elif option=="2":
-        print("option not available yet")
+        computer()
     elif option=="3":
         print("option not available yet")
     else:
@@ -60,10 +77,32 @@ def game(a,b,gamelen):
         choice.append([achoice,bchoice])
 def play(p,num):
     if p.isdigit():
-        pass
+        p=int(p)
+        if p==1:
+            c=random()
+        elif p==2:
+            c=t4t(num)
+        elif p==3:
+            c=Freidman(num)
+        elif p==4:
+            c=Joss()
+        else:
+            computer()
     else:
         c=player(p)
-        return c
+    return c
+def presentscore(player1,player2):
+    wipe()
+    out()
+    n=score()
+    if n[0]>n[1]:
+        print(f"{player1} won {n[0]}-{n[1]}")
+    elif n[1]>n[0]:
+        print(f"{player2} won {n[1]}-{n[0]}")
+    else:
+        print(f"You drew {n[0]}-{n[1]}")
+    
+#programs
 def player(p):
     wipe()
     print(p)
@@ -75,4 +114,20 @@ def player(p):
     else:
         return player(p)
 def t4t(num):
-    return choice[-1][num-1]
+    if len(choice)>0:
+        return choice[-1][num-1]
+    return "c"
+def random():
+    return chr(rand(99,100))
+def Freidman(num):
+    a=[]
+    for i in choice:
+        a.append(i[num-1])
+    if "d" in a:
+        print("d")
+        return "d"
+    print("c")
+    return "c"
+def Joss():
+    return chr(round(rand(994,999)/10))
+terminal()
